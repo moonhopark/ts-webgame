7b953a68ec20ef9286132769f531c75a89bb3946
moonhopark <moonho4199@naver.com>
2022-03-21 17:03:19 +0900

    feat: gugudan logic

--- gugudan/gugudan.js
+++ gugudan/gugudan.js
@@ -1,3 +1,35 @@
-var a = 1;
-console.log('hello ts');
-console.log(a);
+var numberOne = Math.ceil(Math.random() * 9);
+var numberTwo = Math.ceil(Math.random() * 9);
+var result = numberOne * numberTwo;
+var word = document.createElement('div');
+word.textContent = "".concat(numberOne, " \uACF1\uD558\uAE30 ").concat(numberTwo);
+document.body.appendChild(word);
+var form = document.createElement('form');
+document.body.appendChild(form);
+var input = document.createElement('input');
+input.type = 'number';
+form.append(input);
+var button = document.createElement('button');
+button.textContent = '입력!';
+form.append(button);
+var resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (result === Number(input.value)) {
+        // 문제 맞춘 경우
+        resultDiv.textContent = '딩동댕';
+        numberOne = Math.ceil(Math.random() * 9);
+        numberTwo = Math.ceil(Math.random() * 9);
+        result = numberOne * numberTwo;
+        word.textContent = "".concat(numberOne, " \uACF1\uD558\uAE30 ").concat(numberTwo);
+        input.value = '';
+        input.focus();
+    }
+    else {
+        // 틀린 경우
+        resultDiv.textContent = '땡';
+        input.value = '';
+        input.focus();
+    }
+});
--- gugudan/gugudan.ts
+++ gugudan/gugudan.ts
@@ -1,4 +1,40 @@
-const a: number = 1;
+let numberOne = Math.ceil(Math.random() * 9);
+let numberTwo = Math.ceil(Math.random() * 9);
+let result = numberOne * numberTwo;
 
-console.log('hello ts');
-console.log(a);
+const word = document.createElement('div');
+word.textContent = `${numberOne} 곱하기 ${numberTwo}`;
+document.body.appendChild(word);
+
+const form = document.createElement('form');
+document.body.appendChild(form);
+
+const input = document.createElement('input');
+input.type = 'number';
+form.append(input);
+
+const button = document.createElement('button');
+button.textContent = '입력!';
+form.append(button);
+
+const resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (result === Number(input.value)) {
+    // 문제 맞춘 경우
+    resultDiv.textContent = '딩동댕';
+    numberOne = Math.ceil(Math.random() * 9);
+    numberTwo = Math.ceil(Math.random() * 9);
+    result = numberOne * numberTwo;
+    word.textContent = `${numberOne} 곱하기 ${numberTwo}`;
+    input.value = '';
+    input.focus();
+  } else {
+    // 틀린 경우
+    resultDiv.textContent = '땡';
+    input.value = '';
+    input.focus();
+  }
+});
